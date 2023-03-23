c6726e8a17a723ca57ec105f78204006fe0f7bc1
Fertugrul <110333712+fatmaertugrul1@users.noreply.github.com>
2023-03-23 21:01:14 +0300

    Add files via upload

---
+++ Baslangic.java
@@ -0,0 +1,24 @@
+package giris;
+import java.util.Scanner;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        int a;
+        Scanner input = new Scanner(System.in);
+        System.out.print(" A sayısını giriniz:");
+        a= input.nextInt();
+        System.out.println(a);
+/* nextBooIean(): Kullanıcıdan boolean değişkenlere veri almak için kullanılır
+nextByte(): Kullanıcıdan byte değişkenlere veri almak için kullanılır
+nextlnt(): Kullanıcıdan int değişkenlere veri almak için kullanılır
+nextLine(): Kullanıcıdan String değişkenlere veri almak için kullanılır */
+
+
+
+
+
+
+
+    }
+}
+
---
+++ Notortalamasi.java
@@ -0,0 +1,32 @@
+package giris;
+import java.util.Scanner;
+public class Notortalamasi {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Matematik notunuz:" );
+        mat= inp.nextInt();
+        System.out.print("Fizik notunuz:" );
+        fizik =inp.nextInt();
+        System.out.print("Kimya notunuz:" );
+        kimya = inp.nextInt();
+        System.out.print("Turkçe notunuz:" );
+        turkce = inp.nextInt();
+        System.out.print("Tarih notunuz:" );
+        tarih = inp.nextInt();
+        System.out.print("Muzik notunuz:" );
+        muzik = inp.nextInt();
+
+        int toplam =(mat + fizik + kimya + turkce + tarih + muzik);
+
+        double sonuc= toplam/6.0;
+        System.out.println(sonuc);
+
+
+
+
+    }
+
+
+}
